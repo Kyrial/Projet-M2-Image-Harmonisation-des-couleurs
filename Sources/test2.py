2e85256a69f2edfd089cfd475f7776de03a710ca
Bardin Melvin <melvin.bardin@etu.umontpellier.fr>
2021-11-04 17:41:33 +0100

    ajout fonction triadique harmonique

--- Sources/test2.py
+++ Sources/test2.py
@@ -1,51 +1,8 @@
 import cv2
 import random
 import math
+from imgTools import *
 
-
-#permet d'afficher les pourcentage de progression
-def verbosePourcent(current, valMax):
-    if verbosePourcent.pourcent < int(current*100/valMax):
-        print("\033[A                             \033[A")
-        print( int(current*100/valMax) , "%" )
-        verbosePourcent.pourcent = int(current*100/valMax)
-    if verbosePourcent.pourcent ==100:
-        verbosePourcent.pourcent = 0 # reset le compteur une fois 100% atteint
-verbosePourcent.pourcent=0
-
-
-##
-
-
-#somme l'occurance des pixel voisin de la couleur courante
-def sommeVoisine(histo,color):
-    distanceVoisin = 3
-    voisins = range(-distanceVoisin, distanceVoisin)
-    
-    somme = 0
-    #Parourt les 8 voisins de color
-    for b, g, r in zip(voisins,voisins, voisins):
-        colorV = (color[0] + b, color[1] + g, color[2] + r)
-        if colorV in histo.keys():
-           somme += histo[colorV]
-    return somme
-
-        
-#retourne la distance entre deux couleurs
-def distance(color, couleur):
-    dist = math.sqrt(pow(color[0] - couleur[0], 2) +
-                    pow(color[1] - couleur[1], 2) +
-                    pow(color[2] - couleur[2], 2));
-    return dist
-def distanceComp(color, couleur, comp):
-    dist = abs(color[comp] - couleur[comp]);
-    return dist
-
-def ifMilieux(isMid, color2, color3, k):
-    if (color2[k] > isMid[k] and isMid[k] > color3[k]) or (color2[k] < isMid[k] and isMid[k] < color3[k]):
-        return True
-    else: return False
-    
 
 #trouve la meilleurs harmonisation et effectue la modification de l'image
 def findBestHarmonieCompl(histo, img, verbose = True):
@@ -78,7 +35,9 @@ def findBestHarmonieCompl(histo, img, verbose = True):
     for i in range(0,img.shape[0]):
         for j in range(0,img.shape[1]):
             #calcul de la distance entre le mode et le complémentaire
-            """
+            #on modifie les pixel courant
+
+        
             distColor = distance(mode[0], img[i,j])
             distCompl = distance(modeCompl, img[i,j])
             if distColor < distCompl:
@@ -87,12 +46,10 @@ def findBestHarmonieCompl(histo, img, verbose = True):
             else:
                 color = modeCompl
                 dist = distCompl
-            """
-           
-
-            #on modifie les pixel courant 
+            
+            
             for k in range(3):
-
+                """
                 distColor = distanceComp(mode[0], img[i,j],k)
                 distCompl = distanceComp(modeCompl, img[i,j],k)
                 if distColor < distCompl:
@@ -100,9 +57,8 @@ def findBestHarmonieCompl(histo, img, verbose = True):
                     #dist = distColor
                 else:
                     color = modeCompl
-                    #dist = distCompl
-
-                
+                    #dist = distCompl 
+                """
                 dist = distanceComp(color, img[i,j], k)
                 
                     #plusieur choix de fonction possible ? sqrt ?
@@ -124,14 +80,96 @@ def findBestHarmonieCompl(histo, img, verbose = True):
                 else:
                     img.itemset((i,j,k), img.item(i,j,k)-formule)
             
-#read picture
+
+
+#trouve la meilleurs harmonisation et effectue la modification de l'image
+def findBestHarmonieTriad(histo, img, verbose = True):
+    #le mode correspond a un p
+    #mode = couleur, occurance 
+    mode = (0,0)
+
+    ite = 0
+    for key, value in histo.items():
+        ite+=1
+        color = list(key)
+      #  print(color)
+        #calcul de la couleur complémentaire
+        colorTriad1 = [color[2],color[0],color[1]]
+        colorTriad2 = [color[1],color[2],color[0]]
+        #on prend en compte aussi les voisines
+        somme =sommeVoisine(histo,color) + sommeVoisine(histo, colorTriad1) + sommeVoisine(histo, colorTriad2)
+
+        if somme > mode[1]:
+            mode = (color, somme)
+        if verbose:
+            verbosePourcent(ite, len(histo))
+       # print("de la recherche de la meilleurs harmonie")
+    modeTriad1 = [mode[0][2],mode[0][0],mode[0][1]]
+    modeTriad2 = [mode[0][1],mode[0][2],mode[0][0]]
+    print("couleur :        ", mode[0])
+    print("triadique 1 : ", modeTriad1)
+    print("triadique 1 : ", modeTriad2)
+    print("nbOcc : ", mode[1])    
+    #on harmonise les couleur de l'image
+    for i in range(0,img.shape[0]):
+        for j in range(0,img.shape[1]):
+            #calcul de la distance entre le mode et le complémentaire
+            #on modifie les pixel courant 
+            distColor = distance(mode[0], img[i,j])
+            distTriad1 = distance(modeTriad1, img[i,j])
+            distTriad2 = distance(modeTriad2, img[i,j])
+            
+            if distColor < distTriad2 and distColor < distTriad1:
+                color = mode[0]
+                dist = distColor
+            elif distTriad1 <distTriad2:
+                color = modeTriad1
+                dist = distTriad1
+            else:
+                color = modeTriad2
+                dist = distTriad2                
+            
+            
+            for k in range(3):
+                """
+                distColor = distanceComp(mode[0], img[i,j],k)
+                distCompl = distanceComp(modeCompl, img[i,j],k)
+                if distColor < distCompl:
+                    color = mode[0]
+                    #dist = distColor
+                else:
+                    color = modeCompl
+                    #dist = distCompl 
+                """
+                dist = distanceComp(color, img[i,j], k)
+                
+                    #plusieur choix de fonction possible ? sqrt ?
+                #formule =  min(math.exp(dist/15)-1,20)
+                #formule = math.sqrt(dist)
+                #formule = max(0,math.floor(25/(1+math.exp(-(dist-70)/8))-1 ))   #sigmoïde
+                #formule =  min(math.exp(dist/15)-1,2)
+                pivot =distanceComp(mode[0], modeTriad1,k)/4
+                if dist<pivot:
+                    formule = dist/2
+                if(dist>pivot and not ifMilieux(img[i,j],mode[0],modeTriad1,k)):
+                    formule = pivot/2
+                else:
+                    formule = (pivot/2)-dist/2
+                formule = min(60,max(0,math.floor(formule)))                
+                if color[k] > img[i,j][k]:
+          
+                    img.itemset((i,j,k), img.item(i,j,k)+formule)
+                else:
+                    img.itemset((i,j,k), img.item(i,j,k)-formule)
+
+
 
 #### ATTENTION: ####
 # OPENCV utilise le format BGR (bleu, vert, rouge)
 # pensez a rectifier si nécessaire pour les calculs
 ####
 
-filename = "chat3"
+filename = "cat3"
 img = cv2.imread ("../Images/Inputs/"+filename+".jpg")
 ImgIndex = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -149,7 +187,9 @@ for i in range(0,img.shape[0]):
             histo[tuple(pixel)] = 1
             
 
-findBestHarmonieCompl(histo, img)
+#findBestHarmonieCompl(histo, img)
+findBestHarmonieTriad(histo, img)
+
 cv2.imwrite("../Images/Outputs/"+filename+"_palette.jpg", img)
 
 
